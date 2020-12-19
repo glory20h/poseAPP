a8f0bed9fbe6847971e633edd20e4a74899af364
glory20h <glory20h@naver.com>
2020-12-19 15:30:21 +0900

    12/19 - 2

--- test4.py
+++ test4.py
@@ -2,12 +2,12 @@ import numpy as np
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 
-# elbow1 = np.load('./elbow_1.npy')
-# elbow2 = np.load('./elbow_2.npy')
-#
-# distance, path = fastdtw(elbow1, elbow2, dist=euclidean)
-# print(distance)
-# print(path[0])
+elbow1 = np.load('./elbow_1.npy')
+elbow2 = np.load('./elbow_2.npy')
+
+distance, path = fastdtw(elbow1, elbow2, dist=euclidean)
+print(distance)
+print(path)
 
 # x = np.array([1, 2, 3, 3, 7])
 # y = np.array([1, 2, 2, 2, 2])
@@ -17,6 +17,6 @@ from scipy.spatial.distance import euclidean
 # print(distance)
 # print(path)
 
-x = np.array([1, 7, 2, 8, 3, 9, 4, 10])
-x_trim = x < 5
-print(x_trim)
+# x = np.array([1, 7, 2, 8, 3, 9, 4, 10])
+# x_trim = x < 5
+# print(x_trim)
