c0db13853abc70bcd3c4f1b460b837eccc96ad11
glory20h <glory20h@naver.com>
2020-12-20 14:49:59 +0900

    12/20

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from pycocotools.coco import COCO
 from requests import Session
 import cv2
 
-from tkinter import *
+import tkinter as tk
 from tkinter import filedialog
+from PIL import Image, ImageTk
 
 APP_KEY = '30c1f56178f37a3d34bc89bb1efeee44'
 
@@ -16,8 +17,9 @@ session = Session()
 session.headers.update({'Authorization': 'KakaoAK ' + APP_KEY})
 
 # Tkinter 창
-root = Tk()
-root.title('Tkinter Test')
+root = tk.Tk()
+root.title('Basketball Pose Correction')
+root.iconbitmap('./basketball.ico')
 
 
 def submit_job_by_file(video_file_path):
@@ -37,7 +39,6 @@ def get_job_result(job_id):
         response = response.json()
         if response['status'] in {'waiting', 'processing'}:
             time.sleep(10)
-            # Put "Loading..." Here?
         else:
             return response
 
@@ -66,6 +67,56 @@ def visualize(resp, threshold=0.2):
         plt.show()
 
 
+# Convert OpenCV img -> PIL ImageTk format for GUI display
+def convert(img):
+    img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_pil = Image.fromarray(img_cvt)
+    img_tk = ImageTk.PhotoImage(image=img_pil)
+    return img_tk
+
+
+#Input : frame num, job_result, file path Output : display skeleton-drawn frame image
+def visualize_cv(frame_num, job_result, path):
+    global img_converted
+    count = 0
+
+    cap = cv2.VideoCapture(path)
+    # Read nth frame
+    while count < frame_num:
+        ret, img = cap.read()  # Read next frame
+        count += 1
+
+    # Draw skeleton on read image
+    keypoints = np.asarray(job_result['annotations'][frame_num-1]['objects'][0]['keypoints']).reshape((17, 3))
+
+    # Draw left arm
+    # int(round(x)) -> round it and change it to integer
+    left_arm_pts = np.array([[keypoints[5][0], keypoints[5][1]], [keypoints[7][0], keypoints[7][1]], [keypoints[9][0], keypoints[9][1]]], dtype=np.int32)
+    cv2.polylines(img, [left_arm_pts], False, (0,255,0))
+
+    # Draw right arm
+    right_arm_pts = np.array([[keypoints[6][0], keypoints[6][1]], [keypoints[8][0], keypoints[8][1]], [keypoints[10][0], keypoints[10][1]]], dtype=np.int32)
+    cv2.polylines(img, [right_arm_pts], False, (0,255,0))
+
+    # Draw left leg
+    left_leg_pts = np.array([[keypoints[11][0], keypoints[11][1]], [keypoints[13][0], keypoints[13][1]], [keypoints[15][0], keypoints[15][1]]], dtype=np.int32)
+    cv2.polylines(img, [left_leg_pts], False, (0,255,0))
+
+    # Draw right leg
+    right_leg_pts = np.array([[keypoints[12][0], keypoints[12][1]], [keypoints[14][0], keypoints[14][1]], [keypoints[16][0], keypoints[16][1]]], dtype=np.int32)
+    cv2.polylines(img, [right_leg_pts], False, (0,255,0))
+
+    # Draw body
+    body_pts = np.array([[keypoints[5][0], keypoints[5][1]], [keypoints[11][0], keypoints[11][1]], [keypoints[12][0], keypoints[12][1]], [keypoints[6][0], keypoints[6][1]]], dtype=np.int32)
+    cv2.polylines(img, [body_pts], True, (0,128,255))
+
+    # img_converted = convert(img)
+    img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_pil = Image.fromarray(img_cvt)
+    img_converted = ImageTk.PhotoImage(image=img_pil)
+    img_display.config(image=img_converted)
+
+
 # Example Use : kneeAngle = calculateAngle(np.array([hipX, hipY]), np.array([kneeX, kneeY]), np.array([ankleX, ankleY]))
 def calculateAngle(a, b, c):
     ba = a - b
@@ -79,25 +130,57 @@ def analyze_video(video_file_path):
     submit_result = submit_job_by_file(video_file_path)
     job_id = submit_result['job_id']
     job_result = get_job_result(job_id)
-    print(job_result)
-    # keypoints = np.asarray(job_result['annotations'][0]['objects'][0]['keypoints'])
-    # print(keypoints)
-
-
-def exp(video_file_path):
-    cap = cv2.VideoCapture(video_file_path)
-    ret, img = cap.read()
-    if ret:
-        cv2.imshow('Frame', img)
+    print("Video Analyze Successful")
+    return job_result
 
 
 def find_file():
     root.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("mp4 files", "*.mp4"), ("all files", "*.*")))
     file_path = root.filename
-    analyze_video(file_path)
-    # exp(file_path)
+    # img_display.config(image=loading_img) Loading... 은 일단 나중에 생각
+    job_result = analyze_video(file_path)
+    visualize_cv(1, job_result, file_path)
 
 
-my_btn = Button(root, text="Open File", command=find_file).pack()
+init_img = ImageTk.PhotoImage(Image.open("./init.png"))
+loading_img = ImageTk.PhotoImage(Image.open("./loading.png"))
+
+info_label = tk.Label(root, text="Feedback goes here", padx=5, pady=20)
+display_frame = tk.LabelFrame(root)
+find_file_btn = tk.Button(root, text="Open File", command=find_file, padx=5, pady=5)
+
+img_display = tk.Label(display_frame, image=init_img)
+img_display.pack()
+
+info_label.pack(side="top", fill="x")
+display_frame.pack(expand=True, fill="both", padx=5, pady=5)
+find_file_btn.pack(side="bottom", pady=10)
+
 
 root.mainloop()
+
+
+
+
+# class DisplayFrame(tk.Frame):
+#     def __init__(self, parent, *args, **kwargs):
+#         super().__init__(parent, *args, **kwargs)
+#         init_img = ImageTk.PhotoImage(Image.open("./init.png"))
+#         loading_img = ImageTk.PhotoImage(Image.open("./loading.png"))
+#
+#         img_display = tk.Label(display_frame, image=init_img)
+#         img_display.pack()
+#
+# def main():
+#     root = tk.Tk()
+#     root.title('Basketball Pose Correction')
+#     root.geometry("1200x700")
+#     root.iconbitmap('./basketball.ico')
+#     info_label = tk.Label(root, text="Feedback goes here", padx=5, pady=20)
+#     display_frame = DisplayFrame(root)
+#     find_file_btn = tk.Button(root, text="Open File", command=find_file, padx=5, pady=5)
+#
+#     root.mainloop()
+#
+# if __name__ = '__main__':
+#     main()
--- test.py
+++ test.py
@@ -1,19 +1,76 @@
-from tkinter import *
-from PIL import ImageTk, Image
-from tkinter import filedialog
-
-root = Tk()
-root.title('Tkinter Test')
-
-def open():
-    global my_image
-    root.filename = filedialog.askopenfilename(initialdir="/", title="Select A File", filetypes=(("png files", "*.png"), ("all files", "*.*")))
-    my_label = Label(root, text=root.filename).pack()
-    my_image = ImageTk.PhotoImage(Image.open(root.filename))
-    my_image_label = Label(image=my_image).pack()
+import cv2
+from PIL import Image, ImageTk
+import numpy as np
+import tkinter as tk
 
 
-my_btn = Button(root, text="Open File", command=open).pack()
+def convert(img):
+    img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_pil = Image.fromarray(img_cvt)
+    img_tk = ImageTk.PhotoImage(image=img_pil)
+    return img_tk
+
+
+def do_stuff():
+    img_display.config(image=img_converted)
+
+cap = cv2.VideoCapture('1.mp4')
+
+ret, img = cap.read()  # Read next frame
+
+# Draw left arm
+# left_arm_pts = np.array([[keypoints[5][0], keypoints[5][1]], [keypoints[7][0], keypoints[7][1]], [keypoints[9][0], keypoints[9][1]]], dtype=np.int32)
+pts = np.array([[207,302], [221,344], [268,350]], dtype=np.int32)
+cv2.polylines(img, [pts], False, (0,255,0))
+
+
+
+# img_converted.show()
+
+root = tk.Tk()
+root.title('Basketball Pose Correction')
+root.iconbitmap('./basketball.ico')
+
+init_img = ImageTk.PhotoImage(Image.open("./init.png"))
+
+img_display = tk.Label(root, image=init_img)
+img_display.pack()
+
+img_converted = convert(img)
+do_stuff()
 
 root.mainloop()
 
+# import cv2
+# from PIL import Image, ImageTk
+# import numpy as np
+# import tkinter as tk
+#
+# def convert(img):
+#     img_cvt = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+#     img_pil = Image.fromarray(img_cvt)
+#     img_tk = ImageTk.PhotoImage(image=img_pil)
+#     return img_tk
+#
+# cap = cv2.VideoCapture('1.mp4')
+#
+# ret, img = cap.read()  # Read next frame
+#
+#
+# # Draw left arm
+# # left_arm_pts = np.array([[keypoints[5][0], keypoints[5][1]], [keypoints[7][0], keypoints[7][1]], [keypoints[9][0], keypoints[9][1]]], dtype=np.int32)
+# pts = np.array([[207,302], [221,344], [268,350]], dtype=np.int32)
+# cv2.polylines(img, [pts], False, (0,255,0))
+#
+# img_converted = convert(img)
+#
+# # img_converted.show()
+#
+# root = tk.Tk()
+# root.title('Basketball Pose Correction')
+# root.iconbitmap('./basketball.ico')
+#
+# display = tk.Label(root, image=img_converted)
+# display.pack()
+#
+# root.mainloop()
